41a7ebc5b0917d4f29666e3094b5713a5bc6d3d2
Taha Yasser Adnan <110527009+tahadnan@users.noreply.github.com>
2024-05-26 18:16:28 +0100

    yd.sh

--- yd.sh
+++ yd.sh
@@ -5,9 +5,6 @@ echo -n "In which Format (mp3/mp4): "
 read format
 echo -n "Specify the dir to download in: "
 read dir
-# if [$format == mp3];then
-#     yt-dlp -o "$dir" -x --audio-format mp3 $vurl
-# fi #I used mp3 instead of "mp3"
 
 if [ "$format" == "mp3" ]; then
     yt-dlp -o "$dir/%(title)s.%(ext)s" -x --audio-format mp3 "$vurl"
